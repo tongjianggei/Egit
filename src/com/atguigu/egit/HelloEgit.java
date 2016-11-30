13106394f628c6e1278748de8baf50f4cbece9a6
tongjianggei <tongjiangfei@126.com>
2016-11-30 21:12:10 +0800

    update 01

--- src/com/atguigu/egit/HelloEgit.java
+++ src/com/atguigu/egit/HelloEgit.java
@@ -4,5 +4,7 @@ public class HelloEgit {
 	public static void main(String[] args) {
 		System.out.println("Hello Egit ......");
 		System.out.println("22222222222");
+		System.out.println("22222222222");
+		
 	}
 }
