c3e3208d31a0a69f47837dd6b540810f9d6d22ad
tongjiangfei <tongjiangfei@126.com>
2016-11-30 21:05:10 +0800

    create 01

---
+++ src/com/atguigu/egit/HelloEgit.java
@@ -0,0 +1,8 @@
+package com.atguigu.egit;
+
+public class HelloEgit {
+	public static void main(String[] args) {
+		System.out.println("Hello Egit ......");
+		System.out.println("22222222222");
+	}
+}
